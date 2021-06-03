0313a22226e216b292a2dcc2df4d1f88b754397b
Miguel Arenas <marenas@servinf.local>
2021-06-02 20:24:03 -0500

    Creando clase Exception personalizada

--- src/main/java/com/ideas/springboot/error/app/controllers/AppController.java
+++ src/main/java/com/ideas/springboot/error/app/controllers/AppController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.ideas.springboot.error.app.errors.UsuarioNoEncontradoException;
 import com.ideas.springboot.error.app.models.domain.Usuario;
 import com.ideas.springboot.error.app.services.UsuarioService;
 
@@ -26,6 +27,9 @@ public class AppController {
 	@GetMapping("ver/{id}")
 	public String ver( @PathVariable Integer id, Model model ) {
 		Usuario usuario = usuarioService.obtenerPorId(id);
+		
+		if( usuario == null ) throw new UsuarioNoEncontradoException( id.toString() );
+		
 		model.addAttribute( "usuario", usuario );
 		model.addAttribute( "titulo", "Detalle usuario: ".concat( usuario.getNombre() ) );
 		return "ver";
